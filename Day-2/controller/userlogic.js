e0dfd1fdfbbe7a5ce244a5c3c3cc16da4f59c5a9
abhikumar26 <abhinavkumarprithvi7@gmail.com>
2026-07-06 14:31:05 +0530

    changes

--- Day-2/controller/userlogic.js
+++ Day-2/controller/userlogic.js
@@ -1,47 +1,59 @@
 import User from "../model/user.js";
 
 
-const createUser = async (req,res)=>{
-    try{
+const createUser = async (req, res) => {
+    try {
         console.log('log data');
-        
-        const {name,email,empID} = req.body
 
-        console.log(name,email, empID);
-        
+        const { name, email, empID } = req.body
 
-        if(!name || !email || empID){
+        console.log(name, email, empID);
+
+
+        if (!name || !email || empID) {
             return res.status(404).json({
-                message:"data not found"
+                message: "data not found"
             })
         }
         // data creation--
-        const user = await User.create({name,email,empID})
+        const user = await User.create({ name, email, empID })
         console.log(user);
 
         // send Response to user--
         res.status(200).json({
-            success:true,
-            message:"data created successfully"
+            success: true,
+            message: "data created successfully"
         })
-        
-    } catch(error){
+
+    } catch (error) {
         res.status(500).json({
-             success:true,
-            message:"data has not been created ",error
+            success: true,
+            message: "data has not been created ", error
 
         })
     }
 }
-const getuser = async(req,res)=>{
-    try{
+const getuser = async (req, res) => {
+    try {
         const user = await user.find()
-        if(user){
+        if (user) {
             return res.status(404).json({
-                message:"data not found"
+                message: "data not found",
+                success: false
+            })
+            res.status(200).json({
+                success: true,
+                user
             })
         }
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'server:error', error
+        })
+
     }
 }
 
-export {createUser};
+
+export { createUser, getuser };
